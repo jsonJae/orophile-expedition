7066ebded0cb45fb148d9244ae8b3ceaadbf3750
Jason Duran <jbduran1@up.edu.ph>
2026-04-02 20:31:45 +0800

    chore: separated the router and the controllers from index.js

--- backend/index.js
+++ backend/index.js
@@ -1,24 +1,20 @@
 import express from 'express';
-import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-
+import connectDB from './config/db_connect.js';
+import authRoutes from './routes/auth_route.js'
 dotenv.config();
-const PORT = process.env.port || 8000;
-const URI = process.env.mongodb_uri;
 
+const PORT = process.env.port || 8000;
 const app = express();
 
+// middleware
 app.use(express.json());
 
-mongoose.connect(URI)
-    .then(() => {
-        console.log('Successfully connected to MongoDB!');
+app.use('/api/auth', authRoutes)
 
+connectDB()
+    .then(() => {
         app.listen(PORT, () => {
-            console.log(`Listening to port ${PORT} `);
+        console.log(`[+] Listening to port ${PORT}`);
         });
     })
-    .catch((error) => {
-        console.log('Error connecting to MongoDB: ', error.message);
-        process.exit(1);
-    });
