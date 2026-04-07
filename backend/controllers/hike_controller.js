98cab29e7a0a63a6839cbca233b67057e93f7240
Jason Duran <jbduran1@up.edu.ph>
2026-04-08 00:09:28 +0800

    feat: added controllers for getting and updating specific hike using id. lessen the fields in the res.json of get all hike.

---
+++ backend/controllers/hike_controller.js
@@ -0,0 +1,105 @@
+import Hike from '../models/hike_model.js';
+
+const createHike = async (req,res) => {
+
+    try{
+        const newHike =  new Hike(
+            {
+                title: req.body.title,
+                description: req.body.description,
+                diff_level: req.body.diff_level,
+                location: req.body.location,
+                start_date: req.body.start_date,
+                end_date: req.body.end_date,
+                price: req.body.price,
+                max_capacity: req.body.max_capacity,
+                curr_capacity: req.body.curr_capacity,
+                status: req.body.status,
+                itinerary: req.body.itinerary,
+                pickup: req.body.pickup,
+                inclusions: req.body.inclusions,
+                exclusions: req.body.exclusions
+            }
+        )
+
+        await newHike.save()
+        res.status(200).json({ created_hike: true, hike: newHike });
+
+    }catch(error){
+        res.status(500).json({ created_hike: false, error: error.message});
+    }
+
+}
+
+const getAllHikes = async (req,res) => {
+
+    try{
+        if(await Hike.countDocuments() === 0){
+            return res.status(200).json({message: 'No hikes available'});
+        }
+
+        const hikes = await Hike.find().select('title diff_level diff_label hike_type location.place start_date end_date price curr_capacity max_capacity').lean();
+
+        res.status(200).json({ hikes: hikes });
+
+    }catch(error){
+        res.status(500).json({error: error.message});
+    }
+
+}
+
+const getHike = async (req,res) => {
+    try{
+
+        const { id } = req.params;
+
+        const hike = await Hike.findById(id);
+
+        if(!hike){
+            return res.status(500).json({success: false, message: "No hike available. "});
+        }
+
+        res.status(200).json({success: true, hike: hike })
+
+    }catch(error){
+        res.status(500).json({error: error.message});
+    }
+}
+
+const updateHike = async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        const hike = await Hike.findById(id);
+
+        if (!hike){
+            return res.status(404).json({ updated: false, message: "No hike available." });
+        }
+        
+        hike.title = req.body.new_title;
+        hike.description = req.body.new_description;
+        hike.diff_level = req.body.new_diff_level;
+        hike.location = req.body.new_location;
+        hike.start_date = req.body.new_start_date;
+        hike.end_date = req.body.new_end_date;
+        hike.price = req.body.new_price;
+        hike.max_capacity = req.body.new_max_capacity;
+        hike.curr_capacity = req.body.new_curr_capacity;
+        hike.status = req.body.new_status;
+        hike.itinerary = req.body.new_itinerary;
+        hike.pickup = req.body.new_pickup;
+        hike.inclusions = req.body.new_inclusions;
+        hike.exclusions = req.body.new_exclusions;
+
+        const updatedHike = await hike.save();
+
+        res.status(200).json({ updated: true, hike: updatedHike });
+
+    } catch (error) {
+        res.status(500).json({ updated: false, error: error.message });
+    }
+}
+
+
+
+export { getAllHikes, createHike, getHike, updateHike };
